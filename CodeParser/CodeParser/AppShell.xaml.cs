fb664c482d1b0ed14f964fde62632219f207fbeb
Sluma <bekarevstanislav@gmail.com>
2024-02-28 19:56:30 +0300

    _init repo

---
+++ CodeParser/CodeParser/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeParser
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CodeParser/CodeParser/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CodeParser
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
